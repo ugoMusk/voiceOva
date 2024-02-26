3562208ee95df87d73699723fb142e970126d7e5
ugoMusk <ugobothankgod@gmail.com>
2024-02-25 18:26:01 -0800

    removed the need to add widget to screen manager in voiva module

--- voiva/vapp/voiva.py
+++ voiva/vapp/voiva.py
@@ -3,17 +3,11 @@ import sys
 modelPath = os.path.abspath("../voiva")
 sys.path.append(modelPath)
 from kivy.app import App
-# from layout import create
-# from layout import home
-# from layout import docs
 from wm import WindowManager
 
 class VoivaApp(App):
     def build(self):
         w_manager = WindowManager()
-        # w_manager.add_widget(create.CreateWindow(name='create'))
-        # w_manager.add_widget(home.HomeWindow(name='home'))
-        # w_manager.add_widget(docs.DocsWindow(name='docs'))
         return w_manager
 
 if __name__ == '__main__':
