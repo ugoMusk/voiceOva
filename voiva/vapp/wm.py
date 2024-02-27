a1337004d6faf3e55364ea3494b41af3e3f7cf56
ugoMusk <ugobothankgod@gmail.com>
2024-02-27 22:03:28 +0100

    remove WindowManager Screen Transition

--- voiva/vapp/wm.py
+++ voiva/vapp/wm.py
@@ -1,11 +1,14 @@
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, NoTransition
 
 class WindowManager(ScreenManager):
     def switch_to_create_screen(self):
+        self.transition = NoTransition()
         self.current = 'create'
     
     def switch_to_home_screen(self):
+        self.transition = NoTransition()
         self.current = 'home'
 
     def switch_to_docs_screen(self):
+        self.transition = NoTransition()
         self.current = 'docs'
